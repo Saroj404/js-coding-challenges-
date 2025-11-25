3b9f2909c0d40ca460a789dad4143ec4dd4be964
Saroj404 <shresthasaroj901@gmail.com>
2025-11-25 16:07:33 +0545

    solved named and default export and namespace import

---
+++ ES6-Modules-npm/main.js
@@ -0,0 +1,11 @@
+import { add, sub } from './math.js';
+import g from './greet.js';
+import * as utils from './utils.js';
+
+console.log(add(5, 3)); // 8
+console.log(sub(5, 3)); // 2
+
+console.log(g("Saroj"));
+
+console.log(utils.subtract(10, 4)); 
+console.log(utils.multiply(2, 6));
