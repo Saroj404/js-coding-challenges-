5a86fff864509cc85cec668434b09468be93996b
Saroj404 <shresthasaroj901@gmail.com>
2025-11-25 14:16:23 +0545

    solved Destructuring Spread Rest

---
+++ Destructuring-Spread-Rest/q3.js
@@ -0,0 +1,12 @@
+ // Q3. Rest Parameters Sum
+ // Write  sum(...nums) that returns the sum of unlimited numbers
+ 
+ // Q3. Rest Parameters Sum
+
+function sum(...nums) {
+  return nums.reduce((total, n) => total + n, 0);
+}
+
+console.log(sum(1, 2, 3));        
+console.log(sum(5, 10, 15, 20));  
+console.log(sum());               
